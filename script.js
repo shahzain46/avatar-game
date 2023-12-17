f1dd7efcb939f99a05bca122bd6a55860e937e0c
shahzain46 <shahzain6016@gmail.com>
2023-12-17 12:17:21 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const headColorInput = document.getElementById('head-color');
+const bodyColorInput = document.getElementById('body-color');
+const legsColorInput = document.getElementById('legs-color');
+
+headColorInput.addEventListener('input', () => {
+  document.querySelector('.avatar .head').style.backgroundColor = headColorInput.value;
+});
+
+bodyColorInput.addEventListener('input', () => {
+  document.querySelector('.avatar .body').style.backgroundColor = bodyColorInput.value;
+});
+
+legsColorInput.addEventListener('input', () => {
+  document.querySelector('.avatar .legs').style.backgroundColor = legsColorInput.value;
+});
